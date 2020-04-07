8ec464b299b4c054f1327d3359b8bd7bb0d8be39
Tucker Hembree <bladeofxp@gmail.com>
2020-04-07 19:32:39 -0400

    searching for all employees now functions properly; added console.table and required properly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,11 +12,11 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
-  database: "top_songsDB"
+  password: "root",
+  database: "businessDB",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   runSearch();
 });
@@ -27,35 +28,47 @@ function runSearch() {
       type: "list",
       message: "What would you like to do?",
       choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "exit"
-      ]
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+        "Exit",
+      ],
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
+        case "View All Employees":
+          allEmployees(); 
+          break;
+        case "View All Departments":
+          break;
+        case "View All Roles":
+          break;
+        case "Add Employee":
+          break;
+        case "Add Department":
+          break;
+        case "Add Role":
+          break;
+        case "Update Employee Role":
+          break;
 
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
-
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
-
-      case "Search for a specific song":
-        songSearch();
-        break;
-
-      case "exit":
-        connection.end();
-        break;
+        case "Exit":
+          connection.end();
+          break;
       }
+    });
+}
+
+function allEmployees() {
+  connection.query(
+    "SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager FROM employee e INNER JOIN role ON e.role_id=role.id INNER JOIN department ON role.department_id=department.id LEFT JOIN employee m ON m.id=e.manager_id ORDER BY e.id ASC", 
+    function(err, res){
+      console.table("All Employee Information", res); 
+      runSearch(); 
     });
 }
 
@@ -64,14 +77,21 @@ function artistSearch() {
     .prompt({
       name: "artist",
       type: "input",
-      message: "What artist would you like to search for?"
+      message: "What artist would you like to search for?",
     })
-    .then(function(answer) {
+    .then(function (answer) {
       var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
+      connection.query(query, { artist: answer.artist }, function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+          console.log(
+            "Position: " +
+              res[i].position +
+              " || Song: " +
+              res[i].song +
+              " || Year: " +
+              res[i].year
+          );
         }
         runSearch();
       });
@@ -80,7 +100,7 @@ function artistSearch() {
 
 function multiSearch() {
   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].artist);
@@ -96,28 +116,29 @@ function rangeSearch() {
         name: "start",
         type: "input",
         message: "Enter starting position: ",
-        validate: function(value) {
+        validate: function (value) {
           if (isNaN(value) === false) {
             return true;
           }
           return false;
-        }
+        },
       },
       {
         name: "end",
         type: "input",
         message: "Enter ending position: ",
-        validate: function(value) {
+        validate: function (value) {
           if (isNaN(value) === false) {
             return true;
           }
           return false;
-        }
-      }
+        },
+      },
     ])
-    .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
+    .then(function (answer) {
+      var query =
+        "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+      connection.query(query, [answer.start, answer.end], function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
           console.log(
@@ -141,23 +162,27 @@ function songSearch() {
     .prompt({
       name: "song",
       type: "input",
-      message: "What song would you like to look for?"
+      message: "What song would you like to look for?",
     })
-    .then(function(answer) {
+    .then(function (answer) {
       console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        if (err) throw err;
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
+      connection.query(
+        "SELECT * FROM top5000 WHERE ?",
+        { song: answer.song },
+        function (err, res) {
+          if (err) throw err;
+          console.log(
+            "Position: " +
+              res[0].position +
+              " || Song: " +
+              res[0].song +
+              " || Artist: " +
+              res[0].artist +
+              " || Year: " +
+              res[0].year
+          );
+          runSearch();
+        }
+      );
     });
 }
--- schema.sql
+++ schema.sql
@@ -25,3 +25,12 @@ CREATE TABLE employee (
   manager_id INT, 
   PRIMARY KEY (id)
 );
+
+INSERT INTO department (id, name) 
+VALUES (1, "Management"), (2, "PR"), (3, "Accounting")
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "CEO", 100000, 1), (2, "Chief Secretary", 80000, 1), (3, "Head of Staff", 90000, 2), (4, "Consultant", 60000, 2), (5, "Head of Accounting", 85000, 3), (6, "General Accountant", 60000, 3)
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Paul", "Blart", 1, ), (2, "Linda", "Larson", 2, 1), (3, "Herbert", "Wilson", 3, 2), (4, "Jacob", "Johnson", 5, 2), (5, "Michael", "Homeburt", 6, 4), (6, "Martha", "Marvin", 6, 4)
